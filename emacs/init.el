081bc797ea1c6119afa18f9109bb55913dd0143d
Jeremy Kaplan <jdkaplan@mit.edu>
2016-07-07 13:08:02 -0400

    Change js indent offset in web-mode

--- emacs/init.el
+++ emacs/init.el
@@ -244,7 +244,7 @@ they line up with the line containing the corresponding opening bracket."
           (lambda ()
             (setq web-mode-markup-indent-offset 2)
             (setq web-mode-css-indent-offset 2)
-            (setq web-mode-code-indent-offset 2)
+            (setq web-mode-code-indent-offset 4)
             (setq web-mode-enable-auto-pairing t)
             (setq web-mode-enable-auto-expanding t)
 

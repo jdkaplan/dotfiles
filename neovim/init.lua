0caa9a44e19c50975399eae02e51f0e2e2913024
Jeremy Kaplan <code@jdkaplan.dev>
2024-04-10 11:58:51 -0700

    neovim: Fix formatoptions after ftplugins

--- neovim/init.lua
+++ neovim/init.lua
@@ -28,9 +28,13 @@ vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.smartindent = true
-vim.opt.formatoptions = "crqnlj"
 vim.o.foldenable = false
 vim.o.joinspaces = false
+
+-- Some ftplugin files will add 'o' back in again, so remove it again after the
+-- filetype has been determined.
+vim.opt.formatoptions = "crqnlj"
+vim.cmd([[ autocmd Filetype * setlocal formatoptions-=o ]])
 
 vim.o.wrap = true
 vim.o.linebreak = true

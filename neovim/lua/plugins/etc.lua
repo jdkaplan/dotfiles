8b15f3e9d545f6cfd995ace9bbbf3b7c5b85fdc1
Jeremy Kaplan <code@jdkaplan.dev>
2024-12-06 18:38:47 -0800

    neovim: which-key

--- neovim/lua/plugins/etc.lua
+++ neovim/lua/plugins/etc.lua
@@ -113,4 +113,51 @@ return {
             check_ts = true,
         },
     },
+
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            preset = "helix",
+            keys = {
+                Up = " ",
+                Down = " ",
+                Left = " ",
+                Right = " ",
+                C = "^",
+                M = "M-",
+                D = "W-",
+                S = "S-",
+                CR = "CR",
+                Esc = "Esc",
+                ScrollWheelDown = "⤓",
+                ScrollWheelUp = "⤒",
+                NL = "⏎",
+                BS = "⇐",
+                Space = "Space",
+                Tab = "Tab",
+                F1  = "F1",
+                F2  = "F2",
+                F3  = "F3",
+                F4  = "F4",
+                F5  = "F5",
+                F6  = "F6",
+                F7  = "F7",
+                F8  = "F8",
+                F9  = "F9",
+                F10 = "F10",
+                F11 = "F11",
+                F12 = "F12",
+            },
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    }
 }

8fcd5dc971d73df9590858205a797d0f9657913e
Jeremy Kaplan <code@jdkaplan.dev>
2026-01-06 21:02:53 -0500

    neovim: fmt ide.lua

--- neovim/lua/plugins/ide.lua
+++ neovim/lua/plugins/ide.lua
@@ -443,8 +443,8 @@ return {
     lazy = false,
   },
   {
-      "Apeiros-46B/uiua.vim",
-      lazy = false,
+    "Apeiros-46B/uiua.vim",
+    lazy = false,
   },
   {
     "mason-org/mason-lspconfig.nvim",
@@ -462,23 +462,23 @@ return {
   {
     "nvimtools/none-ls.nvim",
     config = function(_plugin, opts)
-        local null_ls = require("null-ls")
+      local null_ls = require("null-ls")
 
-        local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+      local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
 
-        local prettier_filetypes = {}
-        for k, v in ipairs(null_ls.builtins.formatting.prettier.filetypes) do
-            prettier_filetypes[k] = v
-        end
-        table.insert(prettier_filetypes, "htmldjango") -- Jinja templates
+      local prettier_filetypes = {}
+      for k, v in ipairs(null_ls.builtins.formatting.prettier.filetypes) do
+        prettier_filetypes[k] = v
+      end
+      table.insert(prettier_filetypes, "htmldjango") -- Jinja templates
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.prettier.with({
-                    filetypes = prettier_filetypes,
-                }),
-            },
-        })
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.prettier.with({
+            filetypes = prettier_filetypes,
+          }),
+        },
+      })
     end,
   },
 
